8c98b7b49c9cb9eefd95c092ffc63ff8fc30fe47
Beat Walti <beat_walti@epam.com>
2023-03-22 17:11:50 +0100

    feat: initial Umbraco CMS version with SqlClient

---
+++ src/Umbraco.FotoPatricia/ConfigureStaticFileOptions.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http.Headers;
+using Microsoft.Extensions.Options;
+using Microsoft.Net.Http.Headers;
+using Umbraco.Cms.Core.Configuration.Models;
+using IHostingEnvironment = Umbraco.Cms.Core.Hosting.IHostingEnvironment;
+
+namespace Umbraco.FotoPatricia;
+
+public class ConfigureStaticFileOptions : IConfigureOptions<StaticFileOptions>
+{
+    // These are the extensions of the file types we want to cache (add and remove as you see fit)
+    private static readonly HashSet<string> _cachedFileExtensions = new(StringComparer.OrdinalIgnoreCase)
+    {
+        ".ico",
+        ".css",
+        ".js",
+        ".svg",
+        ".woff2",
+        ".jpg"
+    };
+
+    private readonly string _backOfficePath;
+
+    public ConfigureStaticFileOptions(IOptions<GlobalSettings> globalSettings, IHostingEnvironment hostingEnvironment)
+        => _backOfficePath = globalSettings.Value.GetBackOfficePath(hostingEnvironment);
+
+    public void Configure(StaticFileOptions options)
+        => options.OnPrepareResponse = ctx =>
+        {
+            // Exclude Umbraco backoffice assets
+            if (ctx.Context.Request.Path.StartsWithSegments(_backOfficePath))
+            {
+                return;
+            }
+
+            // Set headers for specific file extensions
+            var fileExtension = Path.GetExtension(ctx.File.Name);
+            if (_cachedFileExtensions.Contains(fileExtension))
+            {
+                ResponseHeaders headers = ctx.Context.Response.GetTypedHeaders();
+
+                // Update or set Cache-Control header
+                CacheControlHeaderValue cacheControl = headers.CacheControl ?? new CacheControlHeaderValue();
+                cacheControl.Public = true;
+                cacheControl.MaxAge = TimeSpan.FromDays(365);
+                headers.CacheControl = cacheControl;
+            }
+        };
+}
---
+++ src/Umbraco.FotoPatricia/Startup.cs
@@ -0,0 +1,103 @@
+using Microsoft.Extensions.Options;
+
+namespace Umbraco.FotoPatricia
+{
+    public class Startup
+    {
+        private readonly IWebHostEnvironment _env;
+        private readonly IConfiguration _config;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup" /> class.
+        /// </summary>
+        /// <param name="webHostEnvironment">The web hosting environment.</param>
+        /// <param name="config">The configuration.</param>
+        /// <remarks>
+        /// Only a few services are possible to be injected here https://github.com/dotnet/aspnetcore/issues/9337.
+        /// </remarks>
+        public Startup(IWebHostEnvironment webHostEnvironment, IConfiguration config)
+        {
+            _env = webHostEnvironment ?? throw new ArgumentNullException(nameof(webHostEnvironment));
+            _config = config ?? throw new ArgumentNullException(nameof(config));
+        }
+
+        /// <summary>
+        /// Configures the services.
+        /// </summary>
+        /// <param name="services">The services.</param>
+        /// <remarks>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940.
+        /// </remarks>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IConfigureOptions<StaticFileOptions>, ConfigureStaticFileOptions>();
+
+            services.AddResponseCompression(options => options.EnableForHttps = true);
+            services.AddResponseCaching();
+
+            services.AddHealthChecks();
+
+            services.AddOutputCache(options =>
+            {
+                options.AddBasePolicy(builder =>
+                {
+                    builder
+                        .Cache()
+                        .Expire(TimeSpan.FromMinutes(5));
+
+                    builder
+                        .With(context => context.HttpContext.Request.Path.StartsWithSegments("/media"))
+                        .Cache()
+                        .Expire(TimeSpan.FromHours(6));
+                    
+                    builder
+                        .With(context => context.HttpContext.Request.Path.StartsWithSegments("/scripts"))
+                        .Cache()
+                        .Expire(TimeSpan.FromHours(6));
+                });
+
+                
+            });
+
+            services.AddUmbraco(_env, _config)
+                .AddBackOffice()
+                .AddWebsite()
+                .AddComposers()
+                .Build();
+        }
+
+        /// <summary>
+        /// Configures the application.
+        /// </summary>
+        /// <param name="app">The application builder.</param>
+        /// <param name="env">The web hosting environment.</param>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseResponseCompression();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseResponseCaching();
+            app.UseOutputCache();
+
+            app.UseHealthChecks("/healthz");
+            
+            app.UseUmbraco()
+                .WithMiddleware(u =>
+                {
+                    u.UseBackOffice();
+                    u.UseWebsite();
+                })
+                .WithEndpoints(u =>
+                {
+                    u.UseInstallerEndpoints();
+                    u.UseBackOfficeEndpoints();
+                    u.UseWebsiteEndpoints();
+                });
+        }
+    }
+}
